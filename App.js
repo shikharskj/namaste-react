2e9b1f52a3ee942abea17f364073ad0f3b0cabda
shikharskj <shikharskj21@gmail.com>
2025-06-20 19:52:57 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1", { id:'heading' }, "Hello world! from React!!!");
+
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+
+root1.render(heading1);
